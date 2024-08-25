e0a6a113eef76aecf55dc3f4e5d66053dc4a0a06
Eric Drejza <edrejza@udel.edu>
2024-08-25 18:46:49 -0400

    Complete Lesson 17 - Router Hooks and Links

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Nav = () => {
+const Nav = ({ search, setSearch }) => {
   return (
-    <nav>
-      <h1>Nav</h1>
+    <nav className='Nav'>
+      <form
+        className='searchForm'
+        action='submit'
+        onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor='search'>Search Posts</label>
+        <input
+          id='search'
+          type='text'
+          placeholder='Search Posts'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        {/* prettier-ignore */}
+        <li><Link to='/'>Home</Link></li>
+        {/* prettier-ignore */}
+        <li><Link to='/post'>Post</Link></li>
+        {/* prettier-ignore */}
+        <li><Link to='/about'>About</Link></li>
+      </ul>
+      <div></div>
     </nav>
   );
 };
